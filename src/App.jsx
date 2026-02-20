dc2adc6b4284508a5a4a1df87bd8adb784b7f04b
karthi3075 <karthijayakumar3007@gmail.com>
2026-02-20 23:31:45 +0530

    Clean up App component by removing unused code
    Removed commented-out imports and components from App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
 import Todo from "./components/Todo";
-
-// import Api from "./components/Api";
-// import Counter from "./components/Counter";
-// import AdminLogin from "./pages/AdminLogin";
-// import ForgetPassword from "./pages/ForgetPassword";
-// import UserLogin from "./pages/UserLogin";
-// import UserRegistration from "./pages/UserRegistration";
-
 function App() {
       return(
         <div>
             <Todo/>
-            {/* <UserLogin/> */}
-            {/* <AdminLogin/> */}
-            {/* <UserRegistration/> */}
-            {/* <ForgetPassword/> */}
-            {/* <Api/> */}
-            {/* <Counter/> */}
-            {/* <Counter/> */}
         </div>
       );
 }
